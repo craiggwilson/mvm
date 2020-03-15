804fc5afa414c4beeacc578d9e10b7f3e404552b
craiggwilson <craiggwilson@gmail.com>
2020-03-14 19:09:53 -0500

    added some color to the logging output.

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,22 @@
 package cmd
 
 import (
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 
+	"github.com/craiggwilson/editline/pkg/editline"
 	"github.com/craiggwilson/mvm/pkg/config"
+	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
 )
-
-func init() {
-	log.SetFlags(0)
-	log.SetOutput(os.Stderr)
-}
 
 var rootOpts RootOptions
 
 func Execute(args []string) {
+	rootCmd.PersistentFlags().BoolVarP(&rootOpts.Verbose, "verbose", "v", false, "turn on verbose logging")
+
 	rootCmd.SetArgs(args)
 	_ = rootCmd.Execute()
 }
@@ -25,9 +25,37 @@ var rootCmd = &cobra.Command{
 	Use:   "mvm",
 	Short: "MongoDB Version Manager",
 	Long:  `MongoDB Version Manager`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// don't log anything other than the message
+		log.SetFlags(0)
+		output := io.Writer(os.Stderr)
+
+		verbose, _ := cmd.Flags().GetBool("verbose")
+
+		editors := []editline.Editor{
+			editline.Prefix("[info] ", editline.EditorFunc(func(line string) (string, editline.Action) {
+				return aurora.Cyan(line[7:]).String(), editline.ReplaceAction
+			})),
+			editline.Prefix("[verbose] ", editline.EditorFunc(func(line string) (string, editline.Action) {
+				if !verbose {
+					return "", editline.RemoveAction
+				}
+
+				return aurora.Green(line[10:]).String(), editline.ReplaceAction
+			})),
+		}
+
+		log.SetOutput(editline.NewWriter(output, editors...))
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		w := log.Writer().(*editline.Writer)
+		_ = w.Flush()
+	},
 }
 
-type RootOptions struct{}
+type RootOptions struct {
+	Verbose bool
+}
 
 // Config returns the configuration for MVM.
 func (o *RootOptions) Config() *config.Config {
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/craiggwilson/mvm
 require (
 	github.com/blang/semver v3.5.1+incompatible
 	github.com/cavaliercoder/grab v2.0.0+incompatible
+	github.com/craiggwilson/editline v0.0.0-20200313140102-7bff4ea64e4f
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.5 // indirect
 )
